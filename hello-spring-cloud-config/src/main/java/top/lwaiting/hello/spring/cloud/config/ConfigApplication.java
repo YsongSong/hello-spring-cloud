8922f0787fc3877275dc7c6c2917dc9ca283e8f2
YsongSong <775575467@qq.com>
2019-05-30 15:54:44 +0800

    add spring-cloud-config-server

---
+++ hello-spring-cloud-config/src/main/java/top/lwaiting/hello/spring/cloud/config/ConfigApplication.java
@@ -0,0 +1,21 @@
+package top.lwaiting.hello.spring.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @ClassName ConfigApplication
+ * @Description TODO
+ * @Author Yang Song
+ * @Date 2019/5/28 16:07
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
