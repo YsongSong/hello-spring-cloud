959eea801831ecc78a217672b67a874b0a68fa0a
YsongSong <775575467@qq.com>
2019-05-31 11:13:15 +0800

    update

---
+++ hello-spring-cloud-zipkin/src/main/java/top/lwaiting/hello/spring/cloud/zipkin/ZipKinApplication.java
@@ -0,0 +1,21 @@
+package top.lwaiting.hello.spring.cloud.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin.server.internal.EnableZipkinServer;
+
+/**
+ * @ClassName ZipKinApplication
+ * @Description TODO
+ * @Author Yang Song
+ * @Date 2019/5/31 10:05
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZipkinServer
+public class ZipKinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipKinApplication.class, args);
+    }
+}
